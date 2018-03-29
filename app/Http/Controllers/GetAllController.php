205d0314264d44e7cb820ad96ebac60278b7aa69
Satit Prontepanon <st_satitza@hotmail.com>
2018-03-29 16:17:53 +0700

    commit
    commit

--- app/Http/Controllers/GetAllController.php
+++ app/Http/Controllers/GetAllController.php
@@ -25,7 +25,7 @@ class GetAllController extends Basecontroller
 
             $where = ['restaurants.active_id' => '1'];
             $restaurants = DB::table('restaurants')
-                ->select('restaurants.id', 'restaurant_name', 'hotel_name', 'actives.active', 'restaurant_comment')
+                ->select('restaurants.id', 'restaurant_name', 'restaurant_email', 'pdf_name', 'hotel_name', 'actives.active', 'restaurant_comment')
                 ->join('hotels', 'restaurants.hotel_id', '=', 'hotels.id')
                 ->join('actives', 'restaurants.active_id', '=', 'actives.id')
                 ->orderBy('restaurants.id', 'asc')->where($where)->get();
